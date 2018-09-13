7189e48171a0b11eb62503c6eb233d8bdda3d7dd
mbiesek <michalbiesek@gmail.com>
2018-09-13 19:46:28 +0200

    using instead of typedef

--- include/pmem_allocator.h
+++ include/pmem_allocator.h
@@ -42,14 +42,13 @@ namespace pmem
     class allocator
     {
     public:
-        typedef T value_type;
-        typedef value_type* pointer;
-        typedef const value_type* const_pointer;
-        typedef value_type& reference;
-        typedef const value_type& const_reference;
-        typedef size_t size_type;
-        typedef ptrdiff_t difference_type;
-
+        using value_type = T;
+        using pointer = value_type*;
+        using const_pointer = const value_type*;
+        using const_reference = const value_type&;
+        using reference = value_type&;
+        using size_type = size_t;
+        using difference_type = ptrdiff_t;
         template<class U>
         struct rebind {
             typedef allocator<U> other;
